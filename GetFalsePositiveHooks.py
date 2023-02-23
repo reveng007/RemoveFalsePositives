0a19a5507a60fd9af6b830512dd4349b908b7648
Soumyani1 <soumyanilbiswas2018@gmail.com>
2023-02-23 16:04:30 +0530

    Add files via upload

---
+++ GetFalsePositiveHooks.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+try:
+    with open(sys.argv[1]) as file:
+        
+        Lines = file.readlines()
+
+        count = 0
+        # Strips the newline character
+        for line in Lines:
+            count += 1
+            hook = line.split(' ')
+
+            chars = list(hook[0])
+
+            print("unsigned char s{}[] = {{ '{}',0x0 }};".format(hook[0], "','".join(map(str, chars))))
+
+except:
+    print("\nUsage: python3 %s <text file full of False Positive Hooked Functions>\n" %sys.argv[0])
+    sys.exit()
